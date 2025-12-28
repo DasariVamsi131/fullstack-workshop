af210f95cfb2effc2089454350716c7fd57cbad4
DasariVamsi131 <vamsidasari131@gmail.com>
2025-12-28 12:11:51 +0530

    Added log-analyzer.sh and sample-log.txt

---
+++ 06-javascript/Exercises/log-analyzer.sh
@@ -0,0 +1,16 @@
+path="/d/fullstack-workshop/06-javascript/Exercises/sample-log.txt"
+
+if [ -e "$path" ]; then
+    echo "exist"
+    echo "the file contains $(wc -l < "$path")lines"
+    echo "Error count:$(grep -c -i 'Error' "$path")"
+    echo "warning count:$(grep -c -i 'warning' "$path")"
+    echo "info count:$(grep -c -i 'info' "$path")"
+    IP_LIST=$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' "$path" | sort | uniq)
+    echo "$IP_LIST"
+
+else
+   echo "not exist"
+fi
+
+
