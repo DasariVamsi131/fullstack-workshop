553133f33cd0b1a7c3028b40aa49382d8fcac0d3
DasariVamsi131 <vamsidasari131@gmail.com>
2026-01-03 09:47:31 +0530

    Added file-organizer.sh

--- 01-linux/challenges/challenge-01/file-organizer.sh
+++ 01-linux/challenges/challenge-01/file-organizer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+# Exit immediately if any command fails
+set -e
+# Check if directory argument is provided
 if [ $# -ne 1 ]; then
   echo "Usage: ./file-organizer.sh /path/to/directory"
   exit 1
@@ -7,26 +9,41 @@ fi
 
 TARGET_DIR="$1"
 
+# Check if the directory exists
+
 if [ ! -d "$TARGET_DIR" ]; then
   echo "Error: Directory does not exist."
   exit 1
 fi
 
+# Move into the target directory
 cd "$TARGET_DIR" || exit 1
 
+
+# Declare an associative array to count files per extension
 declare -A file_count
 
+# Loop through files that contain an extension
 for file in *.*; do
+
+ # Skip if no matching files
   [ -e "$file" ] || continue
 
+  # Extract file extension
   ext="${file##*.}"
 
+
+  # Create directory for the extension if it doesn't exist
   mkdir -p "$ext"
+
+  # Move the file into its extension directory
   mv "$file" "$ext/"
 
+  # Increment file count
   ((file_count[$ext]++))
 done
 
+# Print summary
 for ext in "${!file_count[@]}"; do
   echo "Organized ${file_count[$ext]} .$ext files"
 done
